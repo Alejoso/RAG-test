cbe5e8d95d64be030ae23ed0ac088cc2e7a6d275
Alejoso <atirador1@eafit.edu.co>
2026-02-19 20:49:57 -0500

    Added metadata to each document with Trujillos advancement

--- create_db.py
+++ create_db.py
@@ -12,7 +12,7 @@ import logging
 import json
 load_dotenv()
 
-DATA_PATH = "data/books"
+DATA_PATH = "dataCleanedExample/Laws"
 CHROMA_PATH  = "chroma"
 
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -43,70 +43,54 @@ def add_info_to_chunks(chunk, prefix , suffix):
 
 # Set how we want our chunking to be done
 
-def split_text(documents: list[Document]):
-    logging.info("Creating chunks...")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, # Set the chunk size in characters
-        chunk_overlap=500, # Each chunk is going to have an overlap of 500 characters
-        length_function=len,
-        add_start_index=True,
-    )
-
-    chunks = text_splitter.split_documents(documents)
-    
-    # Print how many documents we got and how many chunks they generated
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-    # See the content of chunk number 10 on the logs
-    logging.info(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-    document = chunks[10]
-    logging.info(
-        "Example of chunk number 10:\n%s\n\n--- METADATA ---\n%s",
-        document.page_content,
-        document.metadata
-    )
-
-    return chunks
+def value_after_colon(s: str) -> str | None:
+    if ": " not in s:
+        return None
+    return s.split(": ", 1)[1]
 
 # Get metadata for chunks
-def get_metadata_arguments(chunks : list[Document]):
-    logging.info("Getting metadata for each chunk...")
+def get_metadata_arguments(documents : list[Document]):
+    logging.info("Getting metadata for each document...")
     metas = []
 
-    for i, doc in enumerate(chunks):
+    for i, doc in enumerate(documents):
         source = doc.metadata.get("source", "")
         source_file = Path(source).name if source else None
+        
+        textMetadata = [l for l in doc.page_content.splitlines() if l.strip()]
+        first5 = textMetadata[:5]
 
-        # Here goes logic for getting the meta data
-        title_i = None
-        signature_i = None
-        date_i = None
-
-        add_info_to_chunks(doc, title_i, signature_i) # Change when we got the original information
-
+        type = value_after_colon(first5[0])
+        number = value_after_colon(first5[1])
+        year = value_after_colon(first5[2])
+        state =value_after_colon(first5[3])
+        entity = value_after_colon(first5[4])
+        
         metas.append({
-            "articulo": title_i,
-            "firma": signature_i,
-            "fecha": date_i,
-            "fuente": source_file,
+            "tipo": type,
+            "numero": number,
+            "año": year,
+            "estado": state,
+            "entidad": entity,
+            "archivo_origen": source_file,
             "chunk_index": i,
         })
 
     return metas
 
 # Add metadata to chunks
-def attach_metadata(chunks : list[Document] , metas : list[dict]):
+def attach_metadata(documents : list[Document] , metas : list[dict]):
     logging.info("Adding metadata...")
 
-    if len(chunks) != len(metas):
-        logging.error(f"Chunks and meta array have different sizes Chunk size: {len(chunks)} Meta size: {len(metas)} ")
-        raise ValueError(f"Chunks and meta array have different sizes \nChunk size: {len(chunks)} \nMeta size: {len(metas)}")
+    if len(documents) != len(metas):
+        logging.error(f"Documents and meta array have different sizes Chunk size: {len(documents)} Meta size: {len(metas)} ")
+        raise ValueError(f"Documents and meta array have different sizes \nChunk size: {len(documents)} \nMeta size: {len(metas)}")
 
-    for doc , meta in zip(chunks , metas):
+    for doc , meta in zip(documents , metas):
         doc.metadata.update(meta)
 
-    logging.info(f"This is how chunks metadata look: {chunks[10].metadata}")
-    return chunks
+    logging.info(f"This is how document metadata look: {documents[0].metadata}")
+    return documents
 
 def save_to_chroma(chunks: list[Document]):
     logging.info("Saving information to db...")
@@ -121,11 +105,37 @@ def save_to_chroma(chunks: list[Document]):
 
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
+def split_text(documents: list[Document]):
+    logging.info("Creating chunks...")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, # Set the chunk size in characters
+        chunk_overlap=500, # Each chunk is going to have an overlap of 500 characters
+        length_function=len,
+        add_start_index=True,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    
+    # Print how many documents we got and how many chunks they generated
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    # See the content of chunk number 10 on the logs
+    logging.info(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    # document = chunks[10]
+    # logging.info(
+    #     "Example of chunk number 10:\n%s\n\n--- METADATA ---\n%s",
+    #     document.page_content,
+    #     document.metadata
+    # )
+
+    return chunks
+
 def main():
     documents = load_documents()
-    raw_chunks = split_text(documents)
-    metas = get_metadata_arguments(raw_chunks)
-    chunks = attach_metadata(raw_chunks , metas)
+    metas = get_metadata_arguments(documents)
+    documentsWithMeta = attach_metadata(documents , metas)
+    chunks = split_text(documentsWithMeta)
     save_to_chroma(chunks)
 
     # Test for looking at the db and how this thingy is saved
@@ -141,8 +151,8 @@ if __name__ == "__main__":
     try:
         main()
         logging.info("Finished :)")
-    except:
-        logging.error("Something went bad :(")
+    except Exception as e:
+        logging.error(f"Something went bad :( \n{e}")
     
 
 
